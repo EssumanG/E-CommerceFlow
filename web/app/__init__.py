6d34fd6fb3381e829b86160ad06bbec5d6192d5c
EssumanG <kojosayves7@gmail.com>
2025-02-13 12:08:03 +0000

    add redis caching

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -2,14 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_migrate import Migrate #Import Flask-Migrate
 from app.flask_config import Config
+from flask_caching import Cache
+import redis
 
 
 db = SQLAlchemy()
-
+cache = Cache()
+redis_client = None
 
 def create_app():
+    global redis_client
     app = Flask(__name__)
     app.config.from_object(Config)
+   
+    cache.init_app(app)
+
+    redis_client = redis.StrictRedis.from_url(app.config['CACHE_REDIS_URL'], decode_responses=True)
 
     db.init_app(app)
     Migrate(app, db) 
--- web/app/routes.py
+++ web/app/routes.py
@@ -1,19 +1,15 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, current_app
 from app.models import Ecommerce, Products
 from sqlalchemy import func, distinct
+from app import db, redis_client
+import json, threading
+import asyncio
 main = Blueprint("main", __name__)
-from app import db
-
-@main.route("/")
-def hello():
-    return render_template("home.html")
 
 
-@main.route("/overview")
-def hello_world():
-    columns = [col.name for col in Ecommerce.__table__.columns if not col.primary_key]
-
-    # Generate unique count results
+async def compute_summary():
+    with current_app.app_context():
+        columns = [col.name for col in Ecommerce.__table__.columns if not col.primary_key]
     unique_counts_list = [
         {
             "column_name": column,
@@ -21,6 +17,28 @@ def hello_world():
         }
         for column in columns
     ]
+
+    redis_client.set('summary', json.dumps(unique_counts_list))
+
+@main.route("/")
+async def hello():
+    asyncio.create_task(compute_summary())
+    return render_template("home.html")
+
+
+@main.route("/overview")
+async def hello_world():
+    
+    # Generate unique count results
+    cache_response = redis_client.get('summary')
+    if cache_response:
+        print("Cached")
+        unique_counts_list = json.loads(cache_response)
+        
+    else:
+        await compute_summary()  # Compute and store it in Redis
+        cache_response = redis_client.get('summary')
+        unique_counts_list = json.loads(cache_response) if cache_response else []
 
     return render_template("index.html", columns=unique_counts_list)
 
