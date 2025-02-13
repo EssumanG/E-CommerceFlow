177a5b38522875090c21aa1957834b7a4bbeb9ac
EssumanG <kojosayves7@gmail.com>
2025-02-13 14:36:26 +0000

    updated port for flask app-2

--- web/main.py
+++ web/main.py
@@ -4,5 +4,4 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Ensure it uses the correct port
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8000)
